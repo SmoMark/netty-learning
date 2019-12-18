95513adeaef1cd09bf768fa7f9d8337c5ef51035
smomark <SJHMark@163.com>
2019-12-18 19:50:57 +0800

    #comment SimpleChannelInboundHandler自动释放内存引用

--- src/main/java/com/smomark/netty/echo/EchoClientHandler.java
+++ src/main/java/com/smomark/netty/echo/EchoClientHandler.java
@@ -9,7 +9,7 @@ import io.netty.util.CharsetUtil;
 
 @ChannelHandler.Sharable
 public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
-
+    private ByteBuf msg;
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
@@ -18,11 +18,19 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
         System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
+        this.msg = msg;
+        System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         ctx.close();
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        super.channelUnregistered(ctx);
+        System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
     }
 }
